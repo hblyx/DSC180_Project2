4b4d9fc49263ca8d72f8744d13e3674f7453e9e6
hblyx <yal064@ucsd.edu>
2023-01-20 11:39:20 -0800

    improve read_graph

--- src/data/read_graph.py
+++ src/data/read_graph.py
@@ -13,21 +13,11 @@ def read_ground_truth(path: str, nodewise: bool=False) -> dict:
         lines = f.readlines()
         for line in lines:
             for node in line.strip().split("\t"):
-                ground_truth[com_idx].append(node)
-                
+                if nodewise:
+                    ground_truth[node].append(com_idx)
+                else:
+                    ground_truth[com_idx].append(node)
+                    
             com_idx += 1
     
-    if nodewise:
-        ground_truth = convert_ground_truth_nodewise(ground_truth)        
-    
     return ground_truth
-
-
-def convert_ground_truth_nodewise(ground_truth: dict) -> dict:
-    output = defaultdict(list)
-    
-    for community in ground_truth:
-        for node in ground_truth[community]:
-            output[node].append(community)
-            
-    return output
