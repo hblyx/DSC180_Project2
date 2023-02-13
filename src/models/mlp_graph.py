61f534091469731966c752ee4341c8c800d2eced
hblyx <yal064@ucsd.edu>
2023-02-12 19:02:17 -0800

    improve the Twitch related parts

--- src/models/mlp_graph.py
+++ src/models/mlp_graph.py
@@ -1,15 +1,17 @@
 import torch
 import torch.nn as nn
-from torch_geometric.utils import to_dense_adj 
+from torch_geometric.utils import to_dense_adj, to_torch_coo_tensor
 
 
 
 class MLPGraph(nn.Module):
-    def __init__(self, num_inputs, num_outputs, seed = None):
+    def __init__(self, num_inputs, num_outputs, seed = None, sparse=False):
         super().__init__()
         if seed != None:
             torch.manual_seed(seed)
             torch.cuda.manual_seed(seed)
+        
+        self.sparse = sparse
 
         self.layers = nn.Sequential(
             nn.Linear(num_inputs, 64),
@@ -21,7 +23,10 @@ class MLPGraph(nn.Module):
 
     def forward(self, graph):
         # this model is a simple MLP that is fed only the adjacency matrix
-        x = to_dense_adj(graph.edge_index).squeeze()
+        if self.sparse:
+            x = to_torch_coo_tensor(graph.edge_index)
+        else:
+            x = to_dense_adj(graph.edge_index).squeeze()
 
         output = self.layers(x)
         return output
